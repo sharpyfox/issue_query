0f37b5280a546f8cfb761ae62faf64f25d049ce0
Vasiliev Nikita <sharpyfox@gmail.com>
2012-06-07 20:49:23 +0400

    add new filter option and delete view patch
    Global changes for issue range plugin.  Delete view patch for
    compatibility with new redmine versions.

--- init.rb
+++ init.rb
@@ -4,17 +4,17 @@ require 'issue_query_patch'
 
 unless Redmine::Plugin.registered_plugins.keys.include?(:redmine_issue_query_operators)
 	Redmine::Plugin.register :redmine_issue_query_operators do
-	  name 'Issue query operators plugin'
-	  author 'Nikita Vasiliev'
-	  author_url 'mailto:sharpyfox@gmail.com'
-	  description 'Issue query operators plugin for Redmine'
-	  version '0.0.1'
-    requires_redmine :version_or_higher => '1.3.0'
+		name 'Issue query operators plugin'
+		author 'Nikita Vasiliev'
+		author_url 'mailto:sharpyfox@gmail.com'
+		description 'Redmine plugin which add filter by issues id'
+		version '0.0.2'
+		requires_redmine :version_or_higher => '1.3.0'
 	end
 end
 
 Dispatcher.to_prepare :redmine_issue_query_operators do
-  require_dependency 'query'
+  require_dependency 'query'    
 
   unless Query.included_modules.include? IssueQuery::Patches::QueryModelPatch
     Query.send(:include, IssueQuery::Patches::QueryModelPatch)
--- lib/issue_query_patch.rb
+++ lib/issue_query_patch.rb
@@ -1,69 +1,47 @@
-module IssueQuery
-  module Patches
-    module QueryModelPatch
-      def self.included(base) # :nodoc:
-        base.extend(ClassMethods)
-        base.send(:include, InstanceMethods)
+module IssueQuery  
+	module Patches		
+		module QueryModelPatch
+			def self.included(base) # :nodoc:
+				base.extend(ClassMethods)
+				base.send(:include, InstanceMethods)
 
-        base.class_eval do
-          alias_method_chain :sql_for_field, :issue_range          
-		  alias_method_chain :available_filters, :issue_range
-
-          class << self            
-			alias_method_chain :operators_by_filter_type, :issue_range
-            alias_method_chain :operators, :issue_range
-          end
-        end
-      end
-      
-      module ClassMethods
-        def operators_with_issue_range
-          o=operators_without_issue_range
-          if o[":"].blank?
-            o[":"]=:label_issue_list
-          end
-          o # return
-        end
-
-        def operators_by_filter_type_with_issue_range
-          o = operators_by_filter_type_without_issue_range
-          #unless o[:list_issue].include?(":")            
-            o[:list_issue] = ":"
-          #end
-          o # return
-        end
-      end
-      
-      module InstanceMethods	  
-		def available_filters_with_issue_range
-          f = available_filters_without_issue_range
-		  f["id"] = { :type => :list_issue, :order => 15 }          
-          f # return
-        end
-
-        def get_issue_range_from_string(issue_str)
-          format_issue = issue_str.to_s.gsub(/\s+/, '')
-		  issue_str.split(",") # return
-        end
-
-        def sql_for_field_with_issue_range(field, operator, value, db_table, db_field, is_custom_filter=false)
-          sql=case operator
-            when ":"
-				case type_for(field)
-					when :list_issue
-						value = get_issue_range_from_string(value.first.strip)
-						sql = "#{db_table}.#{db_field} IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
-					else					
-					# IN an empty set
-						sql = "1=0"
+				base.class_eval do
+					alias_method_chain :available_filters, :issue_range
+				end
 			end
-            else
-              sql_for_field_without_issue_range(field, operator, value, db_table, db_field, is_custom_filter)
-          end
-          sql
-        end        
-      end
-    end
-
-  end
+      
+			module ClassMethods
+				#dummy
+			end
+      
+			module InstanceMethods
+				def available_filters_with_issue_range
+					f = available_filters_without_issue_range
+					f["id"] = { :type => :text, :order => 15 }					
+					f # return
+				end
+				
+				def get_issue_range_from_string(issue_str)
+					format_issue = issue_str.to_s.gsub(/\s+/, '')
+					issue_str.split(",") # return
+				end				
+			
+				def sql_for_id_field(field, operator, value)										
+					case operator
+						when ":", "~"
+							value = get_issue_range_from_string(value.first.strip)
+							sql = "#{Issue.table_name}.id IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
+						when "!~"
+							value = get_issue_range_from_string(value.first.strip)
+							sql = "#{Issue.table_name}.id NOT IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
+						else					
+							# IN an empty set
+							sql = "1=0"
+					end			
+					
+					sql #return
+				end		
+			end        
+		end #module QueryModelPatch
+	end #module Patches		
 end
