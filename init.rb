761de66662f311d6e55ffa2fa1922b31911e4e2d
Nikita Vasiliev <sharpyfox@gmail.com>
2014-05-11 19:22:57 +0400

    Redmine 2.x support

--- init.rb
+++ init.rb
@@ -1,22 +1,25 @@
 require 'redmine'
-require 'dispatcher'
-require 'issue_query_patch'
 
-unless Redmine::Plugin.registered_plugins.keys.include?(:redmine_issue_query_operators)
-	Redmine::Plugin.register :redmine_issue_query_operators do
-		name 'Issue query operators plugin'
-		author 'Nikita Vasiliev'
-		author_url 'mailto:sharpyfox@gmail.com'
-		description 'Redmine plugin which add filter by issues id'
-		version '0.0.2'
-		requires_redmine :version_or_higher => '1.3.0'
-	end
+unless Redmine::Plugin.registered_plugins.keys.include?(:issue_query)
+  Redmine::Plugin.register :issue_query do
+    name 'Issue query operators plugin'
+    author 'Nikita Vasiliev'
+    author_url 'mailto:sharpyfox@gmail.com'
+    description 'Redmine plugin which add filter by issues id'
+    version '0.0.3'
+    requires_redmine :version_or_higher => '1.3.0'
+  end
 end
 
-Dispatcher.to_prepare :redmine_issue_query_operators do
-  require_dependency 'query'    
+# Including dispatcher.rb in case of Rails 2.x
+require 'dispatcher' unless Rails::VERSION::MAJOR >= 3
 
-  unless Query.included_modules.include? IssueQuery::Patches::QueryModelPatch
-    Query.send(:include, IssueQuery::Patches::QueryModelPatch)
+if Rails::VERSION::MAJOR >= 3
+  ActionDispatch::Callbacks.to_prepare do
+    require 'issue_query_patch'
+  end
+else
+  Dispatcher.to_prepare :issue_query do    
+    require 'issue_query_patch'
   end
 end
--- lib/issue_query_patch.rb
+++ lib/issue_query_patch.rb
@@ -1,47 +1,51 @@
-module IssueQuery  
-	module Patches		
-		module QueryModelPatch
-			def self.included(base) # :nodoc:
-				base.extend(ClassMethods)
-				base.send(:include, InstanceMethods)
+require 'application_helper'
+require 'query'
 
-				base.class_eval do
-					alias_method_chain :available_filters, :issue_range
-				end
-			end
-      
-			module ClassMethods
-				#dummy
-			end
-      
-			module InstanceMethods
-				def available_filters_with_issue_range
-					f = available_filters_without_issue_range
-					f["id"] = { :type => :text, :order => 15 }					
-					f # return
-				end
-				
-				def get_issue_range_from_string(issue_str)
-					format_issue = issue_str.to_s.gsub(/\s+/, '')
-					issue_str.split(",") # return
-				end				
-			
-				def sql_for_id_field(field, operator, value)										
-					case operator
-						when ":", "~"
-							value = get_issue_range_from_string(value.first.strip)
-							sql = "#{Issue.table_name}.id IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
-						when "!~"
-							value = get_issue_range_from_string(value.first.strip)
-							sql = "#{Issue.table_name}.id NOT IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
-						else					
-							# IN an empty set
-							sql = "1=0"
-					end			
-					
-					sql #return
-				end		
-			end        
-		end #module QueryModelPatch
-	end #module Patches		
+module CustomIssueQuery  
+  module Patches      
+    module QueryModelPatch
+      def self.included(base) # :nodoc:
+        base.send(:include, InstanceMethods)
+
+        base.class_eval do
+          alias_method_chain :available_filters, :issue_range
+        end
+      end
+
+      module InstanceMethods
+        def available_filters_with_issue_range          
+          f = available_filters_without_issue_range
+          options = { :type => :text, :order => 15 }
+          options[:name] = l("field_id") if Rails::VERSION::MAJOR >= 3
+          f["id"] = options
+          f # return
+        end
+        
+        def get_issue_range_from_string(issue_str)
+          format_issue = issue_str.to_s.gsub(/\s+/, '')
+          issue_str.split(",") # return
+        end             
+    
+        def sql_for_id_field(field, operator, value)                                        
+          case operator
+            when ":", "~"
+              value = get_issue_range_from_string(value.first.strip)
+              sql = "#{Issue.table_name}.id IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
+            when "!~"
+              value = get_issue_range_from_string(value.first.strip)
+              sql = "#{Issue.table_name}.id NOT IN (" + value.collect{|val| "'#{connection.quote_string(val)}'"}.join(",") + ")"
+            else                    
+              # IN an empty set
+              sql = "1=0"
+          end         
+          
+          sql #return
+        end     
+      end        
+    end #module QueryModelPatch
+  end #module Patches
+end
+
+unless Query.included_modules.include? CustomIssueQuery::Patches::QueryModelPatch  
+  Query.send(:include, CustomIssueQuery::Patches::QueryModelPatch)
 end
